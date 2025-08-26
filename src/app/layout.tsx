91d3080f8879987e6fe15a16bf4b3d965f8c6beb
kluivstar <kasykasy610@gmail.com>
2025-08-26 17:40:57 +0100

    Deployment error fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
+// src/app/layout.tsx
 import { Montserrat, Orbitron } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 
-// Default (Poppins)
-export const montserrat = Montserrat({
+// Fonts (do NOT export them)
+const montserrat = Montserrat({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700", "800"],
   variable: "--font-montserrat",
 });
 
-// Special (Orbitron)
 const orbitron = Orbitron({
   subsets: ["latin"],
   weight: ["400", "700", "800"],
@@ -23,14 +23,14 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-     <html lang="en">
+    <html lang="en" className={`${montserrat.variable} ${orbitron.variable}`}>
       <head>
         <link
           href="https://fonts.googleapis.com/css2?family=Sarina&display=swap"
           rel="stylesheet"
         />
       </head>
-      <body className={`${montserrat.variable} ${orbitron.variable} font-sans`}>
+      <body className="font-sans">
         <Header />
         {children}
       </body>
